e0d67f9479f14bb7cf1d99647041b3875f22664c
Matthew <justmatt@outlook.com>
2026-01-17 15:41:02 +0000

    feat: remove unused Waila tooltip integration for Chaos Arena Waystone

--- atm10/kubejs/startup_scripts/chaos_arena.js
+++ atm10/kubejs/startup_scripts/chaos_arena.js
@@ -16,23 +16,4 @@ StartupEvents.registry('block', event => {
     });
 });
 
-StartupEvents.postInit(event => {
-  if (!Platform.isClientEnvironment()) return;
-  const $WailaClientRegistration = Java.loadClass('snownee.jade.impl.WailaClientRegistration');
-  $WailaClientRegistration.instance().addTooltipCollectedCallback(0, (tooltip, accessor) => global.jadeCallback(tooltip, accessor));
-  $WailaClientRegistration.instance().tooltipCollectedCallback.sort();
-});
-
-global.jadeCallback = (boxElement, accessor) => {
-  const $ElementHelper = Java.loadClass("snownee.jade.impl.ui.ElementHelper");
-  const $NumberFormat = Java.loadClass("java.text.NumberFormat");
-  if (!accessor.hitResult || accessor.block.id !== 'chaos_arena:waystone') return;
-  // const tooltip = boxElement.getTooltip();
-  // let component = Text.gray('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
-  // let element = $ElementHelper.INSTANCE.text(component);
-  // console.log(`Before: ${tooltip.size()}`);
-  // tooltip.add(element);
-  // console.log(`After: ${tooltip.size()}`);
-};
-
 console.log('Registered Chaos Arena blocks');
